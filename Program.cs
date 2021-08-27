9a0c68f863a87a4769f928a53e9b243f3a981d22
Leandro Gonçalves Vanelli <55560279+LeoVanelli@users.noreply.github.com>
2021-08-26 21:25:10 -0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercício 3 - Faça um programa que exiba os nomes das cidades do litoral sul de São Paulo, um em cada linha.
+
+            string[] cidades = new string[]{"Iguape","Ilha Comprida","Cananéia","Peruíbe","Itanhaém","Mongaguá","Praia Grande","São Vicente","Santos","Guarujá","Cubatão","Bertioga"};
+
+            // Título
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Cidades do litoral sul de São Paulo\n");
+
+            Console.ResetColor(); // Reinicia as cores do título
+
+            // Resolução do exercício
+            foreach (var c in cidades)
+            {
+                Console.WriteLine("{0}\n",c);
+            }
+
+            // Aguardar o usuário fechar o programa ou apertar um botão.
+            Console.Write("Pressione qualquer tecla para fechar. . .");
+            Console.ReadKey();
+        }
+    }
+}
